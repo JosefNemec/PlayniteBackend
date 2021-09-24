55b0af00e41ad00f6055069bd189e7bb926df17d
Josef Nemec <nemecjosef@gmail.com>
2021-09-24 13:32:06 +0200

    Don't send invalid search strings to IGDB

--- source/PlayniteServices/Controllers/IGDB/GamesController.cs
+++ source/PlayniteServices/Controllers/IGDB/GamesController.cs
@@ -66,6 +66,7 @@ namespace PlayniteServices.Controllers.IGDB
             }
 
             List<ulong> searchResult = null;
+            searchString = searchString.Replace("\\", "").Replace("/", "").Trim();
             var modifiedSearchString = ModelsUtils.GetIgdbSearchString(searchString);
             var filter = Builders<IgdbSearchResult>.Filter.Eq(a => a.Id, modifiedSearchString);
             var col = alternativeSearch ? Database.Instance.IgdbAltSearches : Database.Instance.IgdbStdSearches;
