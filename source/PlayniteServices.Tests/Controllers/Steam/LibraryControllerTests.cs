e20820ef33c9a7865479199f952f9f8ad3266c25
Josef Nemec <nemecjosef@gmail.com>
2022-11-14 15:28:10 +0100

    Fixed Steam library test

--- source/PlayniteServices.Tests/Controllers/Steam/LibraryControllerTests.cs
+++ source/PlayniteServices.Tests/Controllers/Steam/LibraryControllerTests.cs
@@ -24,18 +24,13 @@ namespace PlayniteServicesTests.Controllers.Steam
         [Fact]
         public async Task GetLibraryTest()
         {
-            var response = await client.GetAsync("/steam/library/nonexistinguser1234");
+            var response = await client.GetAsync("/steam/library/123456789");
             var errorResponse = JsonConvert.DeserializeObject<GenericResponse>(await response.Content.ReadAsStringAsync());
             Assert.True(!string.IsNullOrEmpty(errorResponse.Error));
             Assert.Null(errorResponse.Data);
 
-            response = await client.GetAsync("/steam/library/playnitedb");
-            var validResponse = JsonConvert.DeserializeObject<ServicesResponse<List<GetOwnedGamesResult.Game>>>(await response.Content.ReadAsStringAsync());
-            Assert.True(string.IsNullOrEmpty(validResponse.Error));
-            Assert.True(validResponse.Data.Count > 0);
-
             response = await client.GetAsync("/steam/library/76561198358889790");
-            validResponse = JsonConvert.DeserializeObject<ServicesResponse<List<GetOwnedGamesResult.Game>>>(await response.Content.ReadAsStringAsync());
+            var validResponse = JsonConvert.DeserializeObject<ServicesResponse<List<GetOwnedGamesResult.Game>>>(await response.Content.ReadAsStringAsync());
             Assert.True(string.IsNullOrEmpty(validResponse.Error));
             Assert.True(validResponse.Data.Count > 0);
         }
