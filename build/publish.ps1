4bd7bab7bdc7d8ad1d10fc64b6f889abcadd8f3a
Josef Nemec <nemecjosef@gmail.com>
2023-10-11 20:32:38 +0200

    publish urls updated

--- build/publish.ps1
+++ build/publish.ps1
@@ -4,9 +4,9 @@ $PSNativeCommandUseErrorActionPreference = $true
 $servicesDir = "/home/playnite/PlayniteServices"
 $tempDir = "/home/playnite/PlayniteServicesNew"
 
-ssh -t playnite@api2.playnite.link "rm -rf $tempDir"
-scp -r .\PlayniteServices\ playnite@api2.playnite.link:$tempDir
-ssh -t playnite@api2.playnite.link "sudo systemctl stop playnite-backend; rm -rf $servicesDir; mv $tempDir $servicesDir; sudo systemctl start playnite-backend"
+ssh -t playnite@api.playnite.link "rm -rf $tempDir"
+scp -r .\PlayniteServices\ playnite@api.playnite.link:$tempDir
+ssh -t playnite@api.playnite.link "sudo systemctl stop playnite-backend; rm -rf $servicesDir; mv $tempDir $servicesDir; sudo systemctl start playnite-backend"
 
 Start-Sleep -s 2
-curl "https://api2.playnite.link/api/playnite/diag/test"
+curl "https://api.playnite.link/api/playnite/diag/test"
